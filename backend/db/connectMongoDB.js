d1f8d9aa1cb56a35ea71670ebaccbfe6315dfd9a
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-29 19:42:47 +0000

    project setup

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+// connection to the database
+const connectMongoDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB connected: ${conn.connection.host}`)
+
+  } catch (error) {
+    console.log(`Error connecting to mongoDB: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+
+export default connectMongoDB;
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import { login, logout, signup } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
-router.get('/signup', (req,res) => {
-  res.json({
-    data: "You hit the signup endpoint!"
-  })
-})
+router.post('/signup', signup)
+
+router.post('/login', login)
+
+router.post('/logout', logout)
+
 
 export default router;
