639f51b52af3b3e048df59e9f87cb47a6a8b6801
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-30 14:52:54 +0000

    logout and getMe done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -88,3 +88,23 @@ export const login = async (req,res) => {
   }
 }
 
+export const logout = async (req,res) => {
+ try {
+  res.cookie("jwt","", {maxAge: 0}); // clear cookie
+  res.status(200).json({message: "Logged out successfully"});
+
+ } catch(error) {
+   console.log("Error in logout controller", error.message);
+   res.status(500).json({ error: "Internal Server Error" });
+ }
+}
+
+export const getMe = async (req,res) => {
+  try {
+    const user = await User.findById(req.user._id) // get user by id from req.user
+
+  } catch (error) {
+    console.log("Error in getMe controller", error.message);
+    res.status(500).json({error: "Internal Server Error"})
+  }
+}
