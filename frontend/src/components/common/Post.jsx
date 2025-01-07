0bf55884528f140cbd54ddc0193269320b614293
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-07 17:45:22 +0000

    add formatPostDate

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 
 const Post = ({ post }) => {
@@ -16,6 +17,14 @@ const Post = ({ post }) => {
 	const {data:authUser} = useQuery({queryKey: ['authUser']})
 	const queryClient = useQueryClient();
 
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser._id); // check if the current user has liked the post
+
+	const isMyPost = authUser._id === post.user._id;// check if the current user is the owner of the post
+
+	const formattedDate = formatPostDate(post.createdAt);
+
+	
 	const {mutate: deletePost, isPending:isDeleting} = useMutation({
 		mutationFn: async() => {
 			try {
@@ -101,13 +110,6 @@ const Post = ({ post }) => {
 			toast.error(error.message)
 		}
 	})
-
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id); // check if the current user has liked the post
-
-	const isMyPost = authUser._id === post.user._id;// check if the current user is the owner of the post
-
-	const formattedDate = "1h";
 
 
 	const handleDeletePost = () => {
